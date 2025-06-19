a633efd401cb0d00bcd2152144c75d7fc3446e13
Techfarms <habeeb.o@aleedconstruction.com>
2025-06-19 17:35:53 +0100

    new update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,8 +12,8 @@ const Footer = () => {
           {/* Company Info */}
           <div className="lg:col-span-2 animate-fade-in">
             <div className="flex items-center space-x-3 mb-6">
-              <div className="w-12 h-12 bg-gradient-to-br from-blue-600 to-blue-700 rounded-xl flex items-center justify-center shadow-lg">
-                <span className="text-white font-bold text-lg font-poppins">CP</span>
+              <div className="w-12 h-12 bg-gradient-to-br from-gray-800 to-gray-700 rounded-xl flex items-center justify-center shadow-lg">
+                <img src="/favicon.png" alt="CICO Pipes Logo" className="w-8 h-8" />
               </div>
               <div>
                 <h3 className="text-2xl font-bold font-poppins">CICO PIPES</h3>
